3515c11cc932bfd7ad168214b544dfaccb4409c9
SriNag <sridharnagireddy@gmail.com>
2025-08-16 21:08:08 +1000

    try-2

--- runmytests.py
+++ runmytests.py
@@ -1,5 +1,20 @@
+
 import pytest
+import os
+import sys
 
 print(f'Hi, PyCharm - Running tests')  # Press Ctrl+F8 to toggle the breakpoint.
-retcode = pytest.main()
 
+
+
+
+#Run all tests in the repository root folder.
+dir=os.path.dirname(os.path.realpath('__file__'))
+os.chdir(dir)
+
+# Skip writing pyc files on a readonly filesystem.
+sys.dont_write_bytecode = True
+
+ret_code=pytest.main([".", "-p", "no:cacheprovider","-v"])
+
+assert ret_code==0, "Pytest failed"
