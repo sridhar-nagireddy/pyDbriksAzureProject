6be0744aa9969ad00293cf6031d15320af4f73b8
SriNag <sridharnagireddy@gmail.com>
2025-08-16 19:30:57 +1000

    run pytests from main.py

--- sritests/test_sri_script.py
+++ sritests/test_sri_script.py
@@ -1,8 +1,16 @@
-import unittest
+# test_my_module.py
+import sys
+sys.path.append('.\srcode')
+from sri_dev_module import   add, subtract
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+    assert subtract(10, 10) == 0
+    assert subtract(0, 5) == -5
