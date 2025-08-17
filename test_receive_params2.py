a1b37d194e0caf849f1ed9d00db11bdc66580884
SriNag <sridharnagireddy@gmail.com>
2025-08-17 20:20:04 +1000

    using ArgParser try to print

--- test_receive_params2.py
+++ test_receive_params2.py
@@ -1,8 +1,11 @@
 import argparse
 
+
 def test_usingArgParser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--filestogagelocation", type=str, help="A parameter from Databricks")
     args = parser.parse_args()
     param_value = args.filestogagelocation
     assert param_value == "stogrageaccountName"
+    print('f passed from databricks is '+{param_value})
+    assert 3 == 4
